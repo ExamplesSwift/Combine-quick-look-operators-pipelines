c903fffb88e81e4395cf6e8ed1c47c2d5ef9c0e4
Henry AT <yiperu@gmail.com>
2020-05-09 19:29:47 -0400

    Use map

--- Combine-quick look-OperatorsAndPipeline.playground/Contents.swift
+++ Combine-quick look-OperatorsAndPipeline.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
+import Combine
 
-var str = "Hello, playground"
+let answer = 41
+
+let _ = Just(answer)
+  .map { value -> String in
+    switch value {
+    case _ where value != 42:
+      return "Absolutely wrong!"
+    default:
+      return "Correct! The answer to life, the universe, and everything!"
+    }
+}
+  .sink { receivedValue in
+    print("Your answer is \(receivedValue)")
+}
+
+
